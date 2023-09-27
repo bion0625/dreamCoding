f6ef7f9dbde3e264279d2770dd9de9ab514ba16d
이의정 <stxtory@gmail.com>
2023-09-27 20:21:30 +0900

    homework

--- javascript/array.js
+++ javascript/array.js
@@ -62,3 +62,94 @@ fruits.push('🍊');
 console.log(fruits);
 console.log(fruits.indexOf('🍊'));
 console.log(fruits.lastIndexOf('🍊'));
+
+console.clear();
+
+// Q1. make a string out of an array
+{
+    const fruits = ['apple', 'banana', 'orange'];
+    const string = fruits.toString();
+    console.log(string);
+}
+
+// Q2. make an array out of a string
+{
+    const fruits = '🍎, 🥝, 🍌, 🍒';
+    const arr = fruits.split(',');
+    console.log(arr);
+}
+
+// Q3. make this array look like this: [5, 4, 3, 2, 1]
+{
+    const array = [1, 2, 3, 4, 5];
+    const reverse = array.reverse();
+    console.log(reverse);
+}
+
+// Q4. make new array without the first two elements
+{
+    const array = [1, 2, 3, 4, 5];
+    array.splice(0,2);
+    console.log(array);
+}
+
+class Student {
+constructor(name, age, enrolled, score) {
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.score = score;
+}
+}
+const students = [
+    new Student('A', 29, true, 45),
+    new Student('B', 28, false, 80),
+    new Student('C', 30, true, 90),
+    new Student('D', 40, false, 66),
+    new Student('E', 18, true, 88),
+];
+
+// Q5. find a student with the score 90
+{
+    const student = students.find(student => student.score === 90);
+    console.log(student);
+}
+
+// Q6. make an array of enrolled students
+{
+    const enrolledStudents = students.filter(student => student.enrolled === true);
+    console.log(enrolledStudents);
+}
+
+// Q7. make an array containing only the students' scores
+// result should be: [45, 80, 90, 66, 88]
+{
+    const scores = students.map(student => student.score);
+    console.log(scores);
+}
+
+// Q8. check if there is a student with the score lower than 50
+{
+    const check = students.every((student) => student.score > 50);
+    console.log(check);
+}
+
+// Q9. compute students' average score
+{
+    // const total = students.reduce((a, b) => a.score + b.score)
+    // console.log(averageScore);????
+}
+
+// Q10. make a string containing all the scores
+// result should be: '45, 80, 90, 66, 88'
+{
+    const string = students.map(student => student.score).join(',');
+    console.log(string);
+}
+
+// Bonus! do Q10 sorted in ascending order
+// result should be: '45, 66, 80, 88, 90'
+{
+    const sortStudents = students.sort((a, b) => a.score - b.score);
+    console.log(sortStudents);
+}

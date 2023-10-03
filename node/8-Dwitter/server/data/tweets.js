f3a15e060b9229057c9180b4917839fce8e19170
이의정 <stxtory@gmail.com>
2023-10-03 19:05:09 +0900

    code align

--- node/8-Dwitter/server/data/tweets.js
+++ node/8-Dwitter/server/data/tweets.js
@@ -1,11 +1,11 @@
 let tweets = [
     {
-      id: 1,
-      text: '드림코더분들 화이팅',
-      createdAt: Date.now().toString(),
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+        id: 1,
+        text: '드림코더분들 화이팅',
+        createdAt: Date.now().toString(),
+        name: 'Bob',
+        username: 'bob',
+        url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
     },
     {
         id: 2,

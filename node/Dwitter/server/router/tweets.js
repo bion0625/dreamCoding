b627f56d6d087b921c13fcc332b0572e73c3905b
bion0625 <bion0625@gmail.com>
2023-10-05 08:58:36 +0900

    Dwitter validation teacher solution

--- node/Dwitter/server/router/tweets.js
+++ node/Dwitter/server/router/tweets.js
@@ -1,47 +1,36 @@
 import express from 'express';
 import 'express-async-error';
+import { body, param, query } from 'express-validator';
 import * as tweetController from '../controller/tweets.js';
-import { body, param, query, validationResult } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
 
-const validation = (req, res, next) => {
-    const errors = validationResult(req);
-    if(errors.isEmpty()){
-        return next();
-    }
-    return res.status(400).json({ message: errors.array()[0].msg });
-};
+const validateTweet = [
+    body('text').trim().isLength({min: 3}).withMessage('text should be at least 3 charaters'),
+    validate,
+];
 
 // GET /tweets
 // GET /tweets?username=username
-router.get('/', query('username').trim().isLength({min:2}).withMessage('이름은 두 글자 이상'), validation, tweetController.getTweets);
+router.get('/', tweetController.getTweets);
 
 // GET /tweets/:id
-router.get('/:id', param('id').trim().isInt().withMessage('유효한 아이디가 아닙니다'), validation, tweetController.getTweet)
+router.get('/:id', param('id').trim().isInt().withMessage('유효한 아이디가 아닙니다'), validate, tweetController.getTweet)
 
 // POST /tweets
 router.post(
     '/', 
-    [
-        body('name').trim().isLength({min:2}).withMessage('이름은 두 글자 이상'), 
-        body('text').trim().isLength({min:2}).withMessage('내용은 두 글자 이상'), 
-        body('username').trim().isLength({min:2}).withMessage('아이디는 두 글자 이상') , 
-    ],
-    validation,
+    validateTweet,
     tweetController.createTweet);
 
 // PUT /tweets/:id
 router.put(
     '/:id', 
-    [
-        param('id').trim().isInt().withMessage('유효한 아이디가 아닙니다'), 
-        body('text').trim().isLength({min:2}).withMessage('내용은 두 글자 이상'), 
-    ],
-    validation,
+    validateTweet,
     tweetController.updateTweet);
 
 // DELETE /tweets/:id
-router.delete('/:id',param('id').trim().isInt().withMessage('유효한 아이디가 아닙니다'), validation, tweetController.deletTweet);
+router.delete('/:id',param('id').trim().isInt().withMessage('유효한 아이디가 아닙니다'), validate, tweetController.deletTweet);
 
 export default router;

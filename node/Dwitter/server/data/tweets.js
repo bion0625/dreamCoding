5970b1cde6d38c5a5106889a7ce4c9e4865acb68
bion0625 <stxtory@gmail.com>
2023-10-12 15:04:48 +0900

    mongodb auth

--- node/Dwitter/server/data/tweets.js
+++ node/Dwitter/server/data/tweets.js
@@ -1,76 +1,76 @@
-import SQ from 'sequelize';
-import { sequelize } from '../db/database.js';
-import { User } from './auth.js';
-const DataTypes = SQ.DataTypes;
+// import SQ from 'sequelize';
+// import { sequelize } from '../db/database.js';
+// import { User } from './auth.js';
+// const DataTypes = SQ.DataTypes;
 
-const Tweets = sequelize.define('tweet', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
-    text: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-});
-Tweets.belongsTo(User);
+// const Tweets = sequelize.define('tweet', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         autoIncrement: true,
+//         allowNull: false,
+//         primaryKey: true,
+//     },
+//     text: {
+//         type: DataTypes.TEXT,
+//         allowNull: false,
+//     },
+// });
+// Tweets.belongsTo(User);
 
-const Sequelize = SQ.Sequelize;
+// const Sequelize = SQ.Sequelize;
 
-const INCLUDE_USER = {
-    attributes: [
-        'id', 
-        'text', 
-        'createdAt', 
-        'userId', 
-        [Sequelize.col('user.name'), 'name'],
-        [Sequelize.col('user.username'), 'username'],
-        [Sequelize.col('user.url'), 'url'],
-    ],
-    include: {
-        model: User,
-        attributes: [],
-    },
-};
+// const INCLUDE_USER = {
+//     attributes: [
+//         'id', 
+//         'text', 
+//         'createdAt', 
+//         'userId', 
+//         [Sequelize.col('user.name'), 'name'],
+//         [Sequelize.col('user.username'), 'username'],
+//         [Sequelize.col('user.url'), 'url'],
+//     ],
+//     include: {
+//         model: User,
+//         attributes: [],
+//     },
+// };
 
-const ORDER_DESC = {
-    order: [['createdAt', 'DESC']],
-};
+// const ORDER_DESC = {
+//     order: [['createdAt', 'DESC']],
+// };
 
-export async function getAll(){
-    return Tweets.findAll({...INCLUDE_USER, ...ORDER_DESC});
-}
+// export async function getAll(){
+//     return Tweets.findAll({...INCLUDE_USER, ...ORDER_DESC});
+// }
 
-export async function getAllByUsername(username){
-    return Tweets.findAll({...INCLUDE_USER, ...ORDER_DESC, include: {
-        ...INCLUDE_USER.include, where: {username}
-    }});
-}
+// export async function getAllByUsername(username){
+//     return Tweets.findAll({...INCLUDE_USER, ...ORDER_DESC, include: {
+//         ...INCLUDE_USER.include, where: {username}
+//     }});
+// }
 
-export async function getById(id){
-    return Tweets.findOne({
-        where: {id},
-        ...INCLUDE_USER,
-    });
-}
+// export async function getById(id){
+//     return Tweets.findOne({
+//         where: {id},
+//         ...INCLUDE_USER,
+//     });
+// }
 
-export async function create(text, userId){
-    return Tweets.create({text, userId}).then(data => this.getById(data.dataValues.id));
-}
+// export async function create(text, userId){
+//     return Tweets.create({text, userId}).then(data => this.getById(data.dataValues.id));
+// }
 
-export async function update(id, text){
-    return Tweets.findByPk(id, INCLUDE_USER)
-        .then(tweet => {
-            tweet.text = text;
-            return tweet.save();
-        });
-}
+// export async function update(id, text){
+//     return Tweets.findByPk(id, INCLUDE_USER)
+//         .then(tweet => {
+//             tweet.text = text;
+//             return tweet.save();
+//         });
+// }
 
-export async function remove(id){
-    return Tweets.findByPk(id)
-        .then(tweet => {
-            tweet.destroy();
-        })
-}
+// export async function remove(id){
+//     return Tweets.findByPk(id)
+//         .then(tweet => {
+//             tweet.destroy();
+//         })
+// }
--- node/Dwitter/server/db/database.js
+++ node/Dwitter/server/db/database.js
@@ -3,10 +3,21 @@ import SQ from 'sequelize';
 import MongoDb from 'mongodb';
 
 
+let db;
 export const connectDB = async () => {
   return MongoDb.MongoClient.connect(config.db.host)
-    .then(client => client.db());
-} 
+    .then(client => {
+      db = client.db();
+    });
+}
+
+export const getUsers = () => {
+  return db.collection('users');
+};
+
+export const getTweets = () => {
+  return db.collection('tweets');
+};
 
 const { host, user, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {

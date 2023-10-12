65a7f0b2175878298c6d0b10ebbf8ef4a0f5d41c
bion0625 <stxtory@gmail.com>
2023-10-12 11:38:51 +0900

    mongodb code setting

--- node/Dwitter/server/app.js
+++ node/Dwitter/server/app.js
@@ -6,7 +6,7 @@ import tweetRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
 import { config } from './config.js';
 import { initSocket } from './connection/socket.js';
-import { sequelize } from './db/database.js';
+import { connectDB } from './db/database.js';
 
 const app = express();
 
@@ -37,7 +37,8 @@ app.use((error, req, res, next) => {
     res.sendStatus(500);
 });
 
-sequelize.sync().then(() => {
+connectDB().then(db => {
+    console.log('init!', db);
     const server = app.listen(config.host.port);
     initSocket(server);
-});
+}).catch(console.error);
--- node/Dwitter/server/db/database.js
+++ node/Dwitter/server/db/database.js
@@ -1,5 +1,12 @@
 import { config } from '../config.js';
 import SQ from 'sequelize';
+import MongoDb from 'mongodb';
+
+
+export const connectDB = async () => {
+  return MongoDb.MongoClient.connect(config.db.host)
+    .then(client => client.db());
+} 
 
 const { host, user, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
